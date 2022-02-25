33de8bc69fd46334064409386477cebc7f9c8956
damhgey <asas.0113696478>
2022-02-25 14:16:10 +0200

    [ADD] reconcile timeoff button in timeoff

--- hr_settlement/models/hr_leave.py
+++ hr_settlement/models/hr_leave.py
@@ -5,3 +5,17 @@ class HrLeave(models.Model):
     _inherit = 'hr.leave'
 
     is_reconcile = fields.Boolean(string="Is Reconciled")
+    settlement_id = fields.Many2one(comodel_name="hr.settlement", string="Settlement", readonly=True)
+
+    def reconcile_timeoff(self):
+        self.ensure_one()
+
+        settlement_data = {
+            'employee_id': self.employee_id.id,
+            'application_date': fields.Date.today(),
+            'settlement_for': 'timeoff_request',
+            'timeoff_request': self.id,
+            'show_timeoff_request': True,
+        }
+        settlement = self.env['hr.settlement'].create(settlement_data)
+        self.write({'settlement_id': settlement.id})
